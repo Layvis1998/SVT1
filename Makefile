3a4a533c7c5f1db60b7b28110853e2bcb6025c68
Layvis1998 <Layvis@mx>
2021-11-30 13:34:09 +0300

    Working (hopefully) prog

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 prog: main.cpp
-	g++  main.cpp lib/libblas.so lib/libumfpack.a lib/libamd.a -O3 -o prog
+	g++  main.cpp lib/libblas.so lib/libumfpack.a lib/libamd.a -O0 -g -o prog
 clean:
 	rm prog
--- main.cpp
+++ main.cpp
@@ -7,29 +7,26 @@
 #include "Include/umfpack.h"
 using namespace std;
 
-int Neumann_type = 2;   //type of Neumann borders
+int Neumann_type = 1;   //type of Neumann borders
 
-int n = 30;        
+int n = 10;        
 int bn = n - 1;
 int bbn = bn - 1;
+int bbbn = bbn - 1;
 double h = 1.0 / n;
 double sh = h * h;
 int N = n * n;  
 int aN = N + 1;
+int b_N = bn * bn;
 
 int NNZ;
 int* row_index;                       
 int* column_index;
 double* values;
-double* b;
 int cur = 0;
   
 int dx = 1;
-int dy = 10;
-double* G_Dirichlet_top;
-double* G_Dirichlet_right;
-double* G_Neumann_left;
-double* G_Neumann_bottom;
+int dy = 1;
     
 double f(double x, double y)
 {
@@ -44,7 +41,7 @@ double u_answer(double x, double y)
 void No_borders(int i, int j, int k)
 {
   column_index[cur] = (i - 1) * n + j;
-  values[cur] = - dy / sh;
+  values[cur] = - dx / sh;
   cur++;
 
   values[cur] = - dy / sh;
@@ -55,7 +52,7 @@ void No_borders(int i, int j, int k)
   column_index[cur] = i * n + j;
   cur++;
       
-  values[cur] = - dx / sh;
+  values[cur] = - dy / sh;
   column_index[cur] = i * n + (j + 1);
   cur++;
             
@@ -66,123 +63,6 @@ void No_borders(int i, int j, int k)
   row_index[k + 1] = row_index[k] + 5;
 }
 
-void Top_Dirichlet(int i, int j, int k)
-{
-  values[cur] = - dy / sh;
-  column_index[cur] = i * n + (j - 1);
-  cur++;
-      
-  values[cur] = 2 * (dx + dy) / sh;
-  column_index[cur] = i * n + j;
-  cur++;
-      
-  values[cur] = - dx / sh;
-  column_index[cur] = i * n + (j + 1);
-  cur++;
-            
-  values[cur] = - dx / sh;
-  column_index[cur] = (i + 1) * n + j;
-  cur++;
-      
-  row_index[k + 1] = row_index[k] + 4; 
-}
-
-void Right_Dirichlet(int i, int j, int k)
-{
-  column_index[cur] = (i - 1) * n + j;
-  values[cur] = - dy / sh;
-  cur++;
-
-  values[cur] = - dy / sh;
-  column_index[cur] = i * n + (j - 1);
-  cur++;
-
-  values[cur] = 2 * (dx + dy) / sh;
-  column_index[cur] = i * n + j;
-  cur++;
-            
-  values[cur] = - dx / sh;
-  column_index[cur] = (i + 1) * n + j;
-  cur++;
-      
-  row_index[k + 1] = row_index[k] + 4;
-}
-
-void Left_Dirichlet(int i, int j, int k)
-{
-  column_index[cur] = (i - 1) * n + j;
-  values[cur] = - dy / sh;
-  cur++;
-
-  values[cur] = 2 * (dx + dy) / sh;
-  column_index[cur] = i * n + j;
-  cur++;
-      
-  values[cur] = - dx / sh;
-  column_index[cur] = i * n + (j + 1);
-  cur++;
-            
-  values[cur] = - dx / sh;
-  column_index[cur] = (i + 1) * n + j;
-  cur++;
-      
-  row_index[k + 1] = row_index[k] + 4;
-}
-
-void Bottom_Dirichlet(int i, int j, int k)
-{
-  column_index[cur] = (i - 1) * n + j;
-  values[cur] = - dy / sh;
-  cur++;
-
-  values[cur] = - dy / sh;
-  column_index[cur] = i * n + (j - 1);
-  cur++;
-      
-  values[cur] = 2 * (dx + dy) / sh;
-  column_index[cur] = i * n + j;
-  cur++;
-      
-  values[cur] = - dx / sh;
-  column_index[cur] = i * n + (j + 1);
-  cur++;
-    
-  row_index[k + 1] = row_index[k] + 4;
-}
-
-void BR_Dirichlet(int i, int j, int k)
-{
-  column_index[cur] = (i - 1) * n + j;
-  values[cur] = - dy / sh;
-  cur++;
-
-  values[cur] = - dy / sh;
-  column_index[cur] = i * n + (j - 1);
-  cur++;
-      
-  values[cur] = 2 * (dx + dy) / sh;
-  column_index[cur] = i * n + j;
-  cur++;
-      
-  row_index[k + 1] = row_index[k] + 3;
-}
-
-void TL_Dirichlet(int i, int j, int k)
-{
-  values[cur] = 2 * (dx + dy) / sh;
-  column_index[cur] = i * n + j;
-  cur++;      
-      
-  values[cur] = - dx / sh;
-  column_index[cur] = i * n + (j + 1);
-  cur++;
-            
-  values[cur] = - dx / sh;
-  column_index[cur] = (i + 1) * n + j;
-  cur++;
-      
-  row_index[k + 1] = row_index[k] + 3;
-}
 
 void TR_Dirichlet(int i, int j, int k)
 {
@@ -201,31 +81,59 @@ void TR_Dirichlet(int i, int j, int k)
   row_index[k + 1] = row_index[k] + 3;
 }
 
-void BL_Dirichlet(int i, int j, int k)
+void Top_Dirichlet(int i, int j, int k)
+{
+
+  values[cur] = - dx / sh;
+  column_index[cur] = i * n + (j - 1);
+  cur++;
+      
+  values[cur] = 2 * (dx + dy) / sh;
+  column_index[cur] = i * n + j;
+  cur++;
+      
+  values[cur] = - dy / sh;
+  column_index[cur] = i * n + (j + 1);
+  cur++;
+            
+  values[cur] = - dx / sh;
+  column_index[cur] = (i + 1) * n + j;
+  cur++;
+      
+  row_index[k + 1] = row_index[k] + 4; 
+}
+
+void Right_Dirichlet(int i, int j, int k)
 {
   column_index[cur] = (i - 1) * n + j;
+  values[cur] = - dx / sh;
+  cur++;
+
   values[cur] = - dy / sh;
+  column_index[cur] = i * n + (j - 1);
   cur++;
 
   values[cur] = 2 * (dx + dy) / sh;
   column_index[cur] = i * n + j;
   cur++;
-      
+            
   values[cur] = - dx / sh;
-  column_index[cur] = i * n + (j + 1);
+  column_index[cur] = (i + 1) * n + j;
   cur++;
       
-  row_index[k + 1] = row_index[k] + 3;  
+  row_index[k + 1] = row_index[k] + 4;
 }
+
+
 
 void Left_Neumann1(int i, int j, int k)
 {
-  column_index[cur] = 0 + (i - 1) * n;
-  values[cur] = dx / h;
+  column_index[cur] = i * n;
+  values[cur] = - dy / sh;
   cur++;
 
-  column_index[cur] = 0 + i * n;
-  values[cur] = - dx / h;
+  column_index[cur] = i * n + 1;
+  values[cur] =  dy / sh;
   cur++;
   
   row_index[k + 1] = row_index[k] + 2; 
@@ -233,171 +141,143 @@ void Left_Neumann1(int i, int j, int k)
 
 void Bottom_Neumann1(int i, int j, int k)
 {
-  column_index[cur] = bn * n + (j - 1);
-  values[cur] = dy / h;
+  column_index[cur] = bbn * n + j;
+  values[cur] = dx / sh;
   cur++;
 
   column_index[cur] = bn * n + j;
-  values[cur] = - dy / h;
+  values[cur] =  - dx / sh;
   cur++;
   
   row_index[k + 1] = row_index[k] + 2; 
 }
 
-void Bottom_Neumann2(int i, int j, int k)
-{  
-  column_index[cur] = bbn * n;
-  values[cur] = dy / sh;
-  cur++;
+
+void BL_Neumann1(int i, int j, int k)
+{ 
   
-  column_index[cur] = bn * n - 1;
-  values[cur] = dx / sh / 2;
+  column_index[cur] = bbn * n;
+  values[cur] = dx / sh / sqrt(2);
   cur++;
   
   column_index[cur] = bn * n;
-  values[cur] = (dy + dx) / sh;
+  values[cur] = - (dx + dy) / sh / sqrt(2);
   cur++;
   
   column_index[cur] = bn * n + 1;
-  values[cur] = dx / sh / 2;
+  values[cur] =  dy / sh / sqrt(2);
   cur++;
   
-  row_index[k + 1] = row_index[k] + 4;
-}
-
-void BL_Neumann2(int i, int j, int k)
-{ 
-  column_index[cur] = bbn * n;
-  values[cur] = -dy / sh;
-  cur++;
-  
-  column_index[cur] = bn * n - 1;
-  values[cur] = -dx / sh;
-  cur++;
-  
-  column_index[cur] = bn * n;
-  values[cur] = (dy + dx) / sh;
-  cur++;
-    
   row_index[k + 1] = row_index[k] + 3;
 }
 
-void Left_Neumann2(int i, int j, int k)
-{
-  column_index[cur] = (i - 1) * n;
-  values[cur] = - dy / sh / 2;
-  cur++;
-
-  column_index[cur] = i * n;
-  values[cur] = (dx + dy) / sh;
-  cur++;
-
-  column_index[cur] = i * n + 1;
-  values[cur] = -dx / sh;
-  cur++;
-  
-  column_index[cur] = (i + 1) * n;
-  values[cur] = - dy / sh / 2;
-  cur++;
-  
-  row_index[k + 1] = row_index[k] + 4; 
-}
 
 int main()
 {
-  G_Dirichlet_top = new double[n];
-  G_Dirichlet_right = new double[bn];
-  G_Neumann_left = new double[bn];
-  G_Neumann_bottom = new double[bbn];
-  row_index = new int[aN];
-  b = new double[N];    
+  double* G_Dirichlet_top = new double[bbn];
+  double* G_Dirichlet_right = new double[bbn];
+  double* G_Neumann_left = new double[bbn];
+  double* G_Neumann_bottom = new double[bbn];
+  double G_BL = 2000;
+  double G_TR = 0;
+  double G_TL = 2000;
+  double G_BR = 0;
+  double* b = new double[N];   
+  
+  row_index = new int[aN]; 
 
   for (int k = 0; k < N; k++)
-  {
-    int i =  k  / n;    
-    int j = k % n;
-    b[k] = cos(M_PI * i * h) * cos(M_PI * j * h);
-  }
+    b[k] = 0;
   
-  for (int a = 0; a < bn; a++)
-    G_Dirichlet_top[a] = 100;
+  for (int a = 0; a < bbn; a++)
+    G_Dirichlet_top[a] = 0;
     
   for (int a = 0; a < bbn; a++)
-    G_Dirichlet_right[a] = 100;
+    G_Dirichlet_right[a] = 0;
 
-  for (int a = 0; a < bn; a++)
-    G_Neumann_left[a] = 1;
+  for (int a = 0; a < bbn; a++)
+    G_Neumann_left[a] = 2000;
   
   for (int a = 0; a < bbn; a++)
-    G_Neumann_bottom[a] = 1.2;
+    G_Neumann_bottom[a] = 0;
   
-  if (Neumann_type == 1)
-  {
-    NNZ = 5 * bbn*bbn + 2*bn + 2*bbn + 4*bbn + 4*bbn + 3 + 3 + 3;  
-    for (int a = 0; a < n; a++)
-      b[a * n] = G_Neumann_left[a];
-      
-    for (int a = 1; a < bn; a++)
-      b[bn*n + a] = G_Neumann_bottom[a - 1]; 
-  }
-  if (Neumann_type == 2)
-  {
-    NNZ = 5 * bbn*bbn + 4*bn + 4*bbn + 4*bbn + 4*bbn + 3 + 3 + 3;  
-    for (int a = 0; a < n; a++)
-      b[a * n] = b[a * n] / 2 + G_Neumann_left[a] / h; 
+  for (int a = 1; a <= bbn; a++)
+    b[a * n] = G_Neumann_left[a - 1] / h;
+  
+  for (int a = 1; a <= bbn; a++)
+    b[bn*n + a] = G_Neumann_bottom[a - 1] / h; 
+
+  for (int a = 1; a <= bbn; a++)                       
+    b[a * n + bn] += G_Dirichlet_right[a - 1] * dy / sh;
     
-    for (int a = 1; a < bn; a++)      
-      b[bn*n + a] = b[bn*n + a]/2 + G_Neumann_bottom[a - 1] / h; 
-  }
-                     
+  for (int a = 1; a <= bbn; a++)
+    b[a] += G_Dirichlet_top[a - 1] * dx / sh;
+
+  b[0] = G_TL;
+  b[bn] = G_TR;
+  b[n * n - 1] = G_BR;
+  b[bn * n] = G_BL / h;
+
+  NNZ = 5 * bbn*bbn + 2*bbn + 2*bbn + 4*bbn + 4*bbn + 3 + (bn + bn + 1);  
   column_index = new int[NNZ];
-  values = new double[NNZ];
-  
+  values = new double[NNZ];  
   row_index[0] = 0;  
+  
+
   for (int k = 0; k < N; k++)
   {
     int i = k / n;
     int j = k % n;
     
-    if ( (i < bn) && (j < bn) && (i > 0) && (j > 0) ) 
+    
+    if ( (i == 0) && (j >= 0) && (j <= bn) )
+    { 
+      values[cur] = 1;
+      column_index[cur] = i * n + j;
+      cur++;
+      row_index[k + 1] = row_index[k] + 1; 
+    }
+    
+    
+    if ( (j == bn) && (i >= 1) && (i <= bn) )     
+    { 
+      values[cur] = 1;
+      column_index[cur] = i * n + j;
+      cur++;
+      row_index[k + 1] = row_index[k] + 1; 
+    }
+    
+    
+    if ( (i > 1) && (i < bn) && (j > 0) && (j < bbn) ) 
       No_borders(i, j, k);
     
-    if ( (i == 0) && (j >= 1) && (j < bn) )
+    if ( (i == 1) && (j >= 1) && (j < bbn) )
       Top_Dirichlet(i, j, k);
     
-    if ( (j == bn) && (i >= 1) && (i < bn) )
+    if ( (j == bbn) && (i > 1) && (i <= bbn) )
       Right_Dirichlet(i, j, k);   
     
-    if ( (j == 0) && (i >= 1) && (i <= bn) )
-    {
-      if (Neumann_type == 1)
-        Left_Neumann1(i, j, k);
-      else if (i < bn)
-        Left_Neumann2(i, j, k);
-    }
-    
-    if ( (j == 0) && (i == bn) && (Neumann_type != 1))
-      BL_Neumann2(i, j, k);  
-      
-    if ( (i == bn) && (j >= 1) && (j < bn) )
-    {
-      if (Neumann_type == 1)
-        Bottom_Neumann1(i, j, k);
-      else
-        Bottom_Neumann2(i, j, k);
-    }
-    if ( (i == bn) && (j == bn) )  // bottom right corner
-      BR_Dirichlet(i, j, k);
-        
-    if ( (i == 0) && (j == 0) )  //top left corner      
-      TL_Dirichlet(i, j, k);
-    if ( (i == 0) && (j == bn) )  // bottom left corner
+    if ( (i == 1) && (j == bbn) )
       TR_Dirichlet(i, j, k);
+
+
+    if ( (j == 0) && (i >= 1) && (i <= bbn) )
+      Left_Neumann1(i, j, k);
+       
+    if ( (i == bn) && (j >= 1) && (j <= bbn) )
+      Bottom_Neumann1(i, j, k);
+      
+    if ( (j == 0) && (i == bn) )
+      BL_Neumann1(i, j, k);
   }
   
+
   // Writing to file
-  ofstream file("CSR.txt");
+  
+  ofstream file("CSR2.txt");
   file << N << " " << NNZ << endl;
+  
+
   for (int k = 0; k < NNZ; ++k)
     file << values[k] << " ";    
   file << endl;
@@ -416,6 +296,7 @@ int main()
   
   file.close();
 
+
   //Factorizing  
   int start_fact = clock();
   double *u = new double[N];
@@ -428,7 +309,7 @@ int main()
   double time_fact = (double) (clock() - start_fact) / CLOCKS_PER_SEC;
   cout << "time of factorization: ms " << time_fact * 1e3 << endl;
 
-  // Solving
+  //Solving
   int start_solve = clock();
   umfpack_di_solve (UMFPACK_A, row_index, column_index, values, u, b, Numeric, nullptr, nullptr);
   double time_solve = (double )(clock() - start_solve) / CLOCKS_PER_SEC;
@@ -450,6 +331,5 @@ int main()
     cout << u[hght + n - 1] << " ";
     cout<< "] \n";
   }
-
 
 }
